794432cf6658aee7ee4f361ce2eecdb6a6337231
Jacob Hensley <jacobrhensley3@gmail.com>
2020-09-17 15:28:07 -0500

    created views and set up controller to handle path, set up bootstrap for shared layout to match

--- moovyMagicRNM/Controllers/HomeController.cs
+++ moovyMagicRNM/Controllers/HomeController.cs
@@ -11,13 +11,17 @@ namespace moovyMagicRNM.Controllers
     {
         public ActionResult Index()
         {
-            var mvcName = typeof(Controller).Assembly.GetName();
-            var isMono = Type.GetType("Mono.Runtime") != null;
-
-            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
             return View();
+        }
+
+        public ActionResult Movies()
+        {
+            return View( "Movies" );
+        }
+
+        public ActionResult Details()
+        {
+            return View("Details");
         }
     }
 }
