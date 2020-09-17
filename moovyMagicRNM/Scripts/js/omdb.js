551e6a16ee4af7e0abb3639ccb2b47f2c603e065
Jacob Hensley <jacobrhensley3@gmail.com>
2020-09-17 18:13:14 -0500

    create js file to run fetch on function call resulting from search button click

--- moovyMagicRNM/Scripts/js/omdb.js
+++ moovyMagicRNM/Scripts/js/omdb.js
@@ -1,1 +1,26 @@
-﻿
+﻿"use strict";
+let results = [];
+
+
+function getMovies() {
+    let searchParam = document.getElementById('search').value;
+
+    results = [];
+
+    fetch("http://www.omdbapi.com/?apikey=64f993ba&s=" + searchParam)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            function count() {
+
+                data.Search.forEach(movie => {
+                    results.push(movie);
+                });
+                //window.location.href = "/home/movies";
+                console.log(results);
+            };
+            count();
+        });
+
+};
