730075022c4ca88ffafd0ace32bfdcddceff4c6d
Jacob Hensley <jacobrhensley3@gmail.com>
2020-09-18 15:55:33 -0500

    completed local storage array for search results and rendered the as html for results movies paage

---
+++ moovyMagicRNM/Scripts/js/movies.js
@@ -0,0 +1,24 @@
+﻿
+var insert = "";
+for (let i = 0; i <= localResults.length - 1; i++) {
+
+    insert += (`<div class="card my-3 p-2 bg-light col-5" style="width: 627px; height="241";>
+                  <div class="row no-gutters">
+                    <div class="col-md-4">
+                      <img src="${ localResults[i].Poster }" class="card-img" style="height:208px; width:138px; alt="~/Scripts/img/alt-film.jpg">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">${ localResults[i].Title }</h5>
+                        <p class="card-text">${ localResults[i].Year }</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+`);
+}
+
+
+//${ localResults[i].Title } <br>`
+
+document.getElementById("results").innerHTML = insert;
--- moovyMagicRNM/Scripts/js/omdb.js
+++ moovyMagicRNM/Scripts/js/omdb.js
@@ -1,11 +1,13 @@
 ﻿"use strict";
 let results = [];
+var localResults = JSON.parse(localStorage.localResults);
 
 
 function getMovies() {
     let searchParam = document.getElementById('search').value;
 
     results = [];
+    localStorage.removeItem("localResults");
 
     fetch("http://www.omdbapi.com/?apikey=64f993ba&s=" + searchParam)
         .then(function (resp) {
@@ -17,10 +19,16 @@ function getMovies() {
                 data.Search.forEach(movie => {
                     results.push(movie);
                 });
-                //window.location.href = "/home/movies";
-                console.log(results);
+                localStorage.localResults = JSON.stringify(results);
+                window.location.href = "/home/movies";
             };
             count();
+
+            //localResults = JSON.parse(localStorage.localResults);
+            //console.log(localResults);
+
         });
 
 };
+
+console.log(localResults);
